3c44bd310618f1e37de6033eff385554ae7c12ff
Marcos Antonio Ribeiro <maribeiro9600@gmail.com>
2022-03-27 10:05:23 -0300

    Home page begin

---
+++ frontweb/src/pages/Home/index.tsx
@@ -0,0 +1,22 @@
+import { ReactComponent as MainImage } from 'assets/images/car-header1.svg';
+import Navbar from 'components/Navbar';
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="home-container">
+        <div className="home-card">
+          <div className="home-content-container">
+            <h1>O carro perfeito para você</h1>
+          </div>
+          <div className="home-image-container">
+            <MainImage />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
